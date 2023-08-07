541521fa74f8bcd9d190a7dc40a8621268a4d54b
Aleksi Merebashvili <aleksimerebashvili@gmail.com>
2023-08-07 22:37:07 +0400

    finish RateApp component, without functional

--- src/RateApp.js
+++ src/RateApp.js
@@ -1,6 +1,17 @@
 import styles from './rate.module.css';
+import { useState } from 'react';
 
 const RateApp = () => {
+  let grey = 'hsla(216, 12%, 54%, 0.2)';
+  let lightGrey = 'hsl(217, 12%, 63%)';
+  const [style, setStyle] = useState({bgColor: grey,
+    txtColor: lightGrey});
+   const changeStyle =()=>{
+      let lightGrey = 'hsl(217, 12%, 63%)';
+      let white = 'hsl(0, 0%, 100%)';
+      setStyle({bgColor: lightGrey,
+        txtColor: white});
+    }
     return (
       <div className={styles.maindiv}>
         <img src="./images/icon-star.svg" alt="star icon" />
@@ -8,11 +19,11 @@ const RateApp = () => {
         <p className={styles.p}>Please let us know how we did with your support request. 
         All feedback is appreciated to help us improve our offering!</p>
         <ul>
-            <li className='number1'>1</li>
-            <li className='number2'>2</li>
-            <li className='number3'>3</li>
-            <li className='number4'>4</li>
-            <li className='number5'>5</li>
+            <li className='number1' style={{background: style.bgColor, color: style.txtColor}} onClick={changeStyle}>1</li>
+            <li className='number2' style={{background: style.bgColor, color: style.txtColor}} onClick={changeStyle}>2</li>
+            <li className='number3' style={{background: style.bgColor, color: style.txtColor}} onClick={changeStyle}>3</li>
+            <li className='number4' style={{background: style.bgColor, color: style.txtColor}} onClick={changeStyle}>4</li>
+            <li className='number5' style={{background: style.bgColor, color: style.txtColor}} onClick={changeStyle}>5</li>
         </ul>
         <button type="submit">S u b m i t</button>
       </div>
