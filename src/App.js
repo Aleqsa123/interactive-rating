c1388b7a6ce274e55ac20b7c4fd589b7f640d10a
Aleksi Merebashvili <aleksimerebashvili@gmail.com>
2023-08-08 17:29:04 +0400

    add RateApp files, finish rate numbers selection functional with styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import RateApp from './RateApp';
-import Response from './Response';
 
 function App() {
   return (
-  <>
     <RateApp/>
-    <Response/>
-  </>);
+);
 }
 
 export default App;
--- src/RateApp.js
+++ src/RateApp.js
@@ -2,16 +2,21 @@ import styles from './rate.module.css';
 import { useState } from 'react';
 
 const RateApp = () => {
-  let grey = 'hsla(216, 12%, 54%, 0.2)';
-  let lightGrey = 'hsl(217, 12%, 63%)';
-  const [style, setStyle] = useState({bgColor: grey,
-    txtColor: lightGrey});
-   const changeStyle =()=>{
-      let lightGrey = 'hsl(217, 12%, 63%)';
-      let white = 'hsl(0, 0%, 100%)';
-      setStyle({bgColor: lightGrey,
-        txtColor: white});
-    }
+  const [liState, changeLiState] = useState({
+    activeLi: null,
+    lies: [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}]
+  })
+
+  function toggleActive(index) {
+    changeLiState({...liState, activeLi: liState.lies[index]});
+  }
+
+  function toggleStyle(index){
+    if (liState.lies[index] === liState.activeLi){
+      return styles.active;
+    }else{return styles.inactive}
+  }
+
     return (
       <div className={styles.maindiv}>
         <img src="./images/icon-star.svg" alt="star icon" />
@@ -19,11 +24,9 @@ const RateApp = () => {
         <p className={styles.p}>Please let us know how we did with your support request. 
         All feedback is appreciated to help us improve our offering!</p>
         <ul>
-            <li className='number1' style={{background: style.bgColor, color: style.txtColor}} onClick={changeStyle}>1</li>
-            <li className='number2' style={{background: style.bgColor, color: style.txtColor}} onClick={changeStyle}>2</li>
-            <li className='number3' style={{background: style.bgColor, color: style.txtColor}} onClick={changeStyle}>3</li>
-            <li className='number4' style={{background: style.bgColor, color: style.txtColor}} onClick={changeStyle}>4</li>
-            <li className='number5' style={{background: style.bgColor, color: style.txtColor}} onClick={changeStyle}>5</li>
+            {liState.lies.map((element, index) => {
+              return <li key={index} className={toggleStyle(index)} onClick = {() => {toggleActive(index)}}>{liState.lies[index].id}</li>
+            })}
         </ul>
         <button type="submit">S u b m i t</button>
 
